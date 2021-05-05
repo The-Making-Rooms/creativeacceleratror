ebf35751952dfdf464d6be01743c6d025cf6fc6f
Uzair Patel <36827239+DottiestDuke47@users.noreply.github.com>
2021-05-05 15:12:20 +0100

    Update Artist pages

--- assets/js/a_invokeGallery.js
+++ assets/js/a_invokeGallery.js
@@ -3,7 +3,6 @@ const queryString = window.location.search;
 const urlParams = new URLSearchParams(queryString);
 const invoke = urlParams.get('invoke')
 
-
 function invokeGallery() {
     if (invoke == 'True') {
         document.getElementById("first").click();
