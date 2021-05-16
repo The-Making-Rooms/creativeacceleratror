b7ad8e9d76060ab034f7eeb7ae3b33544acb7e3a
Uzair Patel <36827239+DottiestDuke47@users.noreply.github.com>
2021-05-16 19:58:10 +0100

    More updates

--- assets/js/a_invokeGallery.js
+++ assets/js/a_invokeGallery.js
@@ -1,4 +1,29 @@
-/*JS Gallery invoke script*/
+/*JS Gallery invoke script and Video Player*/
+
+window.document.onkeydown = function(e) {
+  if (!e) {
+    e = event;
+  }
+  if (e.keyCode == 27) {
+    lightbox_close();
+  }
+}
+
+function lightbox_open() {
+  var lightBoxVideo = document.getElementById("VisaChipCardVideo");
+  window.scrollTo(0, 0);
+  document.getElementById('light').style.display = 'block';
+  document.getElementById('fade').style.display = 'block';
+  lightBoxVideo.play();
+}
+
+function lightbox_close() {
+  var lightBoxVideo = document.getElementById("VisaChipCardVideo");
+  document.getElementById('light').style.display = 'none';
+  document.getElementById('fade').style.display = 'none';
+  lightBoxVideo.pause();
+}
+
 const queryString = window.location.search;
 const urlParams = new URLSearchParams(queryString);
 const invoke = urlParams.get('invoke')
@@ -7,6 +32,12 @@ function invokeGallery() {
     if (invoke == 'True') {
         document.getElementById("first").click();
       }    
+}
+
+function manualInvokeGallery() {
+    lightbox_close();
+    document.getElementById("second").click();  
+    
 }
 
 window.onload = invokeGallery;
